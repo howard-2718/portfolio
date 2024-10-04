7ed0b3a8911dc3c43ab099acf9d9b3aa0d085dae
howard-2718 <howard.li.2718@gmail.com>
2024-10-03 22:34:41 -0700

    Reformat file layout to appease the build process

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: '/portfolio/',
     build: {
-      rollupOptions: {
+        rollupOptions: {
         input: {
-          main: './index.html',
-          projects: './src/projects.html',  // Make sure to include this
+            main: './index.html',
+            projects: './src/projects.html',  // Make sure to include this
         },
       },
     },
